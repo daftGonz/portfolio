3f26fccaa3716df3353254a1d269f0b87ac0deea
Alex <50020229+daftGonz@users.noreply.github.com>
2025-06-21 16:43:56 -0400

    Attempted syntax fix

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,7 +8,7 @@ export const name = 'Alex';
 export const lastName = 'Gonzalez';
 
 export const description =
-	"I'm a Systems Engineer with a strong focus on Microsoft Azure and AWS cloud architecture. I specialize in designing and implementing secure, scalable systems that prioritize business continuity, disaster recovery, and operational resilience. My approach includes building robust cloud environments, integrating critical services and applications, and aligning infrastructure with organizational goals to maximize performance and minimize downtime. I thrive on solving complex infrastructure challenges and enabling teams to move faster and more securely in the cloud.";
+	'I\'m a Systems Engineer with a strong focus on Microsoft Azure and AWS cloud architecture. I specialize in designing and implementing secure, scalable systems that prioritize business continuity, disaster recovery, and operational resilience. My approach includes building robust cloud environments, integrating critical services and applications, and aligning infrastructure with organizational goals to maximize performance and minimize downtime. I thrive on solving complex infrastructure challenges and enabling teams to move faster and more securely in the cloud.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
 	{ platform: Platform.GitHub, link: 'https://github.com/' },
